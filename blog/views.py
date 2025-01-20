cf7a4d2d862f33bbed88eb8fa99f87dc62610bf3
amaljoy2002 <amaljoy8151@gmail.com>
2025-01-20 12:18:22 +0530

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'post_list.html', {})
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
